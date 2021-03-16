d46688818fb2766c3e50c34a225d576299e98d16
banu tryon <banutryon@banus-mbp.lan>
2021-03-15 19:57:24 -0700

    finished update on about me section.

--- src/components/about.js
+++ src/components/about.js
@@ -6,10 +6,34 @@ function About() {
 		<section className="container" id="about">
 			<h1 className="about">About</h1>
 			<img id="node" src="/Icons/Nodes.png" alt="node" />
+
 			<span className="rock"></span>
-			<span className="dive"></span>
+			<div id="popup1">
+				<p className="p_about">
+					Assessing a problem before deciding on a solution. Select methods that
+					are the most efficient while maintaining the highest quality. "Pareto
+					Principle"
+				</p>
+			</div>
+			<span className="dive "></span>
+			<div id="popup2">
+				<p className="p_about">
+					Dive deep - consider all angles of a problem or need
+				</p>
+			</div>
 			<span className="rocket"></span>
+			<div id="popup3">
+				<p className="p_about">
+					Fast load times to keep the customer engaged is my highest priority.
+				</p>
+			</div>
 			<span className="laptop"></span>
+			<div id="popup4">
+				<p className="p_about">
+					My layouts will work on any device, big or small.
+				</p>
+			</div>
+
 			{/* <div className="aboutContainer">
 				<div className="aboutIconMe">
 					<img src="/Icons/me.png" alt="aboutIcon" />
@@ -52,15 +76,15 @@ function About() {
 			</div> */}
 			<div className="text_container">
 				<span className="text">
+					<Me />
 					<p className="p">
 						As a software engineer, I will take my previous experiences and
 						apply them to the projects at hand and provide a solution to your
-						users that will make your company proud. My passion to improve
+						users that will make your company proud. AMy passion to improve
 						people’s lives through technology will allow me to put in the work
 						to make sure we understand the customer at a high level and create
 						world-class products.
 					</p>
-					<Me />
 
 					<p className="p">
 						Having over 15 years of experience in consumer electronics has
